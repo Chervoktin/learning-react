4c6af0f1e849a791c522bc0b82b5c11af8413407
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-06-10 23:31:01 +0300

    changed TodoItem.js

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -3,30 +3,37 @@ import PropTypes from "prop-types";
 
 const styles = {
   li: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems:'center',
-      padding: '.5rem 1rem',
-      border: '1px solid #ccc',
-      boredRadius: '4px',
-      marginBottom: '.5rem'
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: ".5rem 1rem",
+    border: "1px solid #ccc",
+    boredRadius: "4px",
+    marginBottom: ".5rem",
   },
-  input:{
-    marginRight: '1rem'
+  input: {
+    marginRight: "1rem",
+  },
+};
+
+function TodoItem({ todo, index, onChange,deleteFromList }) {
+  let classes = [];
+  if(todo.completed){
+    classes.push('done');
   }
-}
-
-
-
-function TodoItem({ todo, index }) {
   return (
     <li style={styles.li}>
-      <span>
-        <input type="checkbox" style={styles.input} />
+      <span className={classes.join(' ')}>
+        <input
+          type="checkbox"
+          style={styles.input}
+          checked={todo.completed}
+          onChange={()=>onChange(todo.id)}
+        />
         <strong>{index + 1}</strong>
         {todo.title}
       </span>
-      <button>&times;</button>
+      <button onClick={()=>deleteFromList(todo.id)}>&times;</button>
     </li>
   );
 }
@@ -34,6 +41,7 @@ function TodoItem({ todo, index }) {
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
   index: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
