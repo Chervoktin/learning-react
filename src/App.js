caabc9e397072e090caad55020bd2e45230f5fd9
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-06-10 23:31:18 +0300

    changed App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React from "react";
-import TodoList from './Todo/TodoList'
+import TodoList from "./Todo/TodoList";
 
 function App() {
-  const todos = [
-    {id:1 , completed: false, title: 'Купить хлеб'},
-    {id:2 , completed: false, title: 'Купить масло'},
-    {id:3 , completed: false, title: 'Купить молоко'},
-  ]
+  const [todos, setTodos] = React.useState([
+    { id: 1, completed: false, title: "Купить хлеб" },
+    { id: 2, completed: true, title: "Купить масло" },
+    { id: 3, completed: false, title: "Купить молоко" },
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function deleteFromList(id) {
+    let index = -1;
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].id === id) {
+        index = i;
+      }
+    }
+    if (index > -1) {
+      todos.splice(index, 1);
+    }
+    setTodos(todos.slice());
+  }
+
   return (
     <div className="wrapper">
       <h1>React tutorial</h1>
-
-      <TodoList todos={todos}/>
+      <TodoList
+        todos={todos}
+        onToggle={toggleTodo}
+        deleteFromList={deleteFromList}
+      />
     </div>
   );
 }
