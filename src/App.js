781fdd62ba4efce73a543a22a3e02238dd3bd7a7
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-06-11 23:37:31 +0300

    changed App.js

--- src/App.js
+++ src/App.js
@@ -2,46 +2,29 @@ import React from "react";
 import TodoList from "./Todo/TodoList";
 
 function App() {
-  const [todos, setTodos] = React.useState([
+  let todos = [
     { id: 1, completed: false, title: "Купить хлеб" },
     { id: 2, completed: true, title: "Купить масло" },
     { id: 3, completed: false, title: "Купить молоко" },
-  ]);
-
-  function toggleTodo(id) {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      })
-    );
-  }
-
-  function deleteFromList(id) {
-    let index = -1;
-    for (let i = 0; i < todos.length; i++) {
-      if (todos[i].id === id) {
-        index = i;
-      }
-    }
-    if (index > -1) {
-      todos.splice(index, 1);
-    }
-    setTodos(todos.slice());
-  }
+  ];
+  
+  let todos2 = [
+    { id: 1, completed: false, title: "one" },
+    { id: 2, completed: false, title: "too" },
+    { id: 3, completed: false, title: "three" },
+  ];
 
   return (
     <div className="wrapper">
-      <h1>React tutorial</h1>
-      <TodoList
-        todos={todos}
-        onToggle={toggleTodo}
-        deleteFromList={deleteFromList}
-      />
+      <h1>List 1 </h1>
+      <TodoList todos={todos} />
+      <h1>List 2 </h1>
+      <TodoList todos={todos2} />
+      <h1>List 3 </h1>
+      <TodoList todos={todos2} />
     </div>
   );
 }
+
 
 export default App;
