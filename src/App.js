187baf5e280c125fd04b84a708ee88d4861efb7c
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-06-13 21:45:54 +0300

    changed App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import React from "react";
 import TodoList from "./Todo/TodoList";
 
 function App() {
-  let todos = [
+  let [todos, setTodos] = React.useState( [
     { id: 1, completed: false, title: "Купить хлеб" },
     { id: 2, completed: true, title: "Купить масло" },
     { id: 3, completed: false, title: "Купить молоко" },
-  ];
+  ]);
   
-  let todos2 = [
+  let [todos2, setTodos2] = React.useState( [
     { id: 1, completed: false, title: "one" },
     { id: 2, completed: false, title: "too" },
     { id: 3, completed: false, title: "three" },
-  ];
+  ]);
 
   return (
     <div className="wrapper">
       <h1>List 1 </h1>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} setTodos={setTodos} />
       <h1>List 2 </h1>
-      <TodoList todos={todos2} />
+      <TodoList todos={todos2} setTodos={setTodos2} />
       <h1>List 3 </h1>
-      <TodoList todos={todos2} />
+      <TodoList todos={todos2} setTodos={setTodos2} />
     </div>
   );
 }
