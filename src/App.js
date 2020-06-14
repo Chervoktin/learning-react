bd219940537b7d3d0ac830337f188b3e0314d60f
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-06-14 15:56:21 +0300

    changed App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import TodoList from "./Todo/TodoList";
 import Input from "./Todo/Input";
 
 function App() {
-  let [todos, setTodos] = React.useState([
-  ]);
+  let [todos, setTodos] = React.useState([]);
 
   let [todos2, setTodos2] = React.useState([
     { id: 1, completed: false, title: "one" },
@@ -17,33 +16,52 @@ function App() {
       .then((res) => res.json())
       .then(
         (result) => {
-         setTodos(result);
+          setTodos(result);
         },
         // Примечание: Обрабатывать ошибки необходимо именно здесь
         // вместо блока catch(), чтобы не пропустить
         // исключения из реальных ошибок в компонентах.
         (error) => {
-         
+          alert("ошибка подключения");
         }
       );
   }, []);
 
   let [value, setValue] = React.useState("test");
-
+  
+  if((document.location.pathname === '/index') || (document.location.pathname === '/')){
   return (
     <div className="wrapper">
       <Input value={value} setValue={setValue}></Input>
       <button
         onClick={() => {
           let id = 0;
-          if (todos2.length !== 0) {
-            id = todos2[todos2.length - 1].id;
+          if (todos.length !== 0) {
+            id = todos[todos.length - 1].id;
           }
-          todos2.push({ id: id + 1, completed: false, title: value });
-          setTodos2(todos2.slice());
+          todos.push({ id: id + 1, completed: false, title: value });
+          setTodos(todos.slice());
         }}
       >
         add
+      </button>
+
+      <button
+        onClick={() => {
+          fetch("http://localhost:3001/")
+            .then((res) => res.json())
+            .then(
+              (result) => {
+                setTodos(result);
+              },
+              // Примечание: Обрабатывать ошибки необходимо именно здесь
+              // вместо блока catch(), чтобы не пропустить
+              // исключения из реальных ошибок в компонентах.
+              (error) => {}
+            );
+        }}
+      >
+        update
       </button>
 
       <h1>List 1 </h1>
@@ -53,7 +71,11 @@ function App() {
       <h1>List 3 </h1>
       <TodoList todos={todos2} setTodos={setTodos2} />
     </div>
-  );
+  );}else{
+    return (
+      <h1>404 not found</h1>
+    )
+  }
 }
 
 export default App;
